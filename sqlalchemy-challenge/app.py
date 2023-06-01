3c95549d219226fc6032ff23b58b91123c643526
johnxuereb17 <127439832+johnxuereb17@users.noreply.github.com>
2023-06-01 18:17:59 -0400

    final final upload

---
+++ sqlalchemy-challenge/app.py
@@ -0,0 +1,66 @@
+# Import the dependencies.
+import numpy as np
+import re
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.sql import exists  
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+station = Base.classes.station
+measurement = Base.classes.measurement
+
+# Create our session (link) from Python to the DB
+session = Session(bind=engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    twelve_month_results = (session.query(measurement.date, measurement.tobs)
+                                    .order_by(measurement.date))
+    
+precipitation_dict = {}
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
